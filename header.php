765044b3b44beef6e6feb1326e3ed7bff81e8839
Viktor Ivanov <viktor.ivan0v98@yandex.ru>
2025-06-25 17:00:40 +0300

    styles

---
+++ header.php
@@ -0,0 +1,30 @@
+    
+    <header>
+        <div class="logo"><a href="index.php">Мой сайт</a></div>
+        <nav>
+            <ul>
+                <li><a href="/Figure-calculators.php">Калькуляторы фигур на JavaScript</a></li>
+                <li><a href="/Math-calculators.php">Математические калькуляторы на JavaScript</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+                <li><a href="/Math-calculators.php">Математические</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/header-style.css">
+    </head>
+    <body>
+        
+    </body>
+    </html>
